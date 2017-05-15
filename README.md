01f3627cffebc8acde9d9900f0b7aa848c908737
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-05-15 15:04:52 -0600

    Implemented edit and update functionality

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ## Key requirement: company needs documentation that salaried employees did or did not get overtime each week
 
 ## Models
-- Post -> date:date rationale:text
-- User -> Devise
-- AdminUser -> STI
+- x Post -> date:date rationale:text
+- x User -> Devise
+- x AdminUser -> STI
 
 ## Features:
 - Approval Workflow
@@ -15,4 +15,7 @@
 - Needs to be documented if employee did not log overtime
 
 ## UI:
-- Bootstrap -> formatting
+- x Bootstrap -> formatting
+
+## To Do:
+- Refactor user association integration test in post_spec
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,17 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all
   end
-  def show
-    @post = Post.find(params[:id])
-  end
   def new
     @post = Post.new
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   def create
@@ -21,6 +24,15 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Your post was successfully updated"
+    else
+      render :edit
+    end
+  end
+
 
   private
 
