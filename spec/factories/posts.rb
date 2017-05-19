637e7f7d9cd0b76ad9cf11afded7f9e326149f5c
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-05-19 14:56:23 -0600

    Implemented the over time requested

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,12 +2,14 @@ FactoryGirl.define do
   factory :post do
     date Date.today
     rationale "Some rationale"
+    overtime_request 3.0
     user
   end
 
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some more content"
+    overtime_request 4.5
     user
   end
 
